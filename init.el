66eabccd36d87c3f3b7f606eaad516bee198499f
Mayur Jadhav <mayur@helpshift.com>
2015-08-20 01:40:56 +0530

    Add org mode key bindings.

--- init.el
+++ init.el
@@ -126,3 +126,19 @@
 (setq mac-command-modifier 'meta)
 
 (server-start)
+
+
+;;;
+;;; Org Mode
+;;;
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/packs/user/org-mode/lisp"))
+(add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
+(require 'org)
+;;
+;; Standard key bindings
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+(setenv "PATH" (concat "/usr/local/smlnj/bin:" (getenv "PATH")))
+(setq exec-path (cons "/usr/local/smlnj/bin"  exec-path))

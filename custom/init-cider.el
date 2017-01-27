c9377b131c21a71a5e9abb23a5bfb57b4b46a6ee
Mayur Jadhav <mayur@helpshift.com>
2017-01-27 14:43:57 +0530

    Add indentation and apropos-actions to cider

--- custom/init-cider.el
+++ custom/init-cider.el
@@ -1,7 +1,8 @@
 (eval-after-load 'cider-repl
   '(progn (add-hook 'cider-repl-mode-hook 'enable-paredit-mode)
           (add-hook 'cider-repl-mode-hook 'company-mode)
-          (add-hook 'cider-mode-hook 'turn-on-eldoc-mode)
+          (add-hook 'cider-repl-mode-hook 'eldoc-mode)
+          (add-hook 'cider-mode-hook 'eldoc-mode)
           (add-hook 'cider-mode-hook 'company-mode)
           (setq nrepl-buffer-name-show-port t)
           (setq cider-prefer-local-resources t)
@@ -9,7 +10,12 @@
           (setq cider-prompt-save-file-on-load nil)
           (setq cider-repl-history-size 1000)
           (setq cider-repl-history-file "~/.emacs.d/cider-history.dat")
-          (define-key cider-repl-mode-map (kbd "C-c M-o") 'cider-repl-clear-buffer)))
+          (define-key cider-repl-mode-map (kbd "C-c M-o") 'cider-repl-clear-buffer)
+          (define-key cider-repl-mode-map (kbd "C-M-q") 'prog-indent-sexp)
+          (setq cider-apropos-actions
+                '(("find-def" . cider--find-var)
+                  ("display-doc" . cider-doc-lookup)
+                  ("lookup-on-grimoire" . cider-grimoire-lookup)))))
 
 (provide 'init-cider)
 
